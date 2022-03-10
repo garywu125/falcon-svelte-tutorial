0afe3e063814529576bb3ec53ea21bb5d4ddb7a1
garywu125 <garywu125@gmail.com>
2022-03-10 10:58:40 +0800

    add astro-project svelte-with-tailwindcss

---
+++ astro-project/svelte-with-taiwindcss/base-template/README.md
@@ -0,0 +1,19 @@
+
+# degit
+```
+npx degit garywu125/falcon-svelte-tutorial/astro-project/svelte-with-tailwindcss/base-template <project-directory>
+cd <project-directory>
+npm install
+npm run dev
+
+```
+
+# Astro + Svelte Example
+
+```
+npm init astro -- --template framework-svelte
+```
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/withastro/astro/tree/latest/examples/framework-svelte)
+
+This example showcases Astro working with [Svelte](https://svelte.dev/).
---
+++ astro-project/svelte-with-taiwindcss/base-template/src/components/Daisy.svelte
@@ -0,0 +1,37 @@
+<svelte:head>
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/daisyui@2.0.6/dist/full.css" rel="stylesheet" type="text/css" />
+	<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>	
+</svelte:head>
+
+<script>
+//   import logo from 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/199px-Svelte_Logo.svg.png'
+  import Counter from './Counter.svelte'
+</script>
+
+<main>
+  <!-- TailwindCSS -->
+  <h1 class="text-3xl font-bold underline text-blue-400">
+    Hello world!
+  </h1> 
+<!-- Svelte Component -->
+  <Counter />
+<!-- DaisyUI component -->
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes"></figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="justify-end card-actions">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </div>
+
+</main>
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style> 
