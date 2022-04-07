e706e955e87ba8362743160700cab658a6625304
garywu125 <garywu125@gmail.com>
2022-04-07 16:01:59 +0800

    add daisyui in app.svelte

--- vite-project/svelte-vite-daisyui-template/src/App.svelte
+++ vite-project/svelte-vite-daisyui-template/src/App.svelte
@@ -9,7 +9,7 @@
   <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
     <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={logo}>
     <div class="text-center lg:w-2/3 w-full">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Svelte + Vite + TailwindCSS = 🌸🌸🌸</h1>
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Svelte + Vite + TailwindCSS + DaisyUI = 🌸🌸🌸</h1>
       <p class="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
       <a class="my-8 bg-red-600 text-white p-4 px-8 underline rounded-3xl" href="https://github.com/Lukem121/svelte-vite-tailwind-template">Please ⭐ on GitHub</a>
       <div class="my-8">
